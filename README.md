eaa56516a1825f8cad1ceb1d2dcd18d36f034f23
Paul Frazee <pfrazee@gmail.com>
2015-08-15 15:35:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# pull-ipc
+
+A pull-stream wrapper around electron's ipc channel.
+
+## Usage
+
+Main thread:
+
+```js
+var pullIpc = require('pull-ipc')
+
+// `window` is the BrowserWindow instance you want to communicate with
+var ipcStream = pullIpc(ipc, window, function (err) {
+  console.log('ipc-stream ended', err)
+})
+pull(ipcStream, someOtherStream, ipcStream)
+```
+
+UI thread:
+
+```js
+var pullIpc = require('pull-ipc')
+
+var ipcStream = pullIpc(ipc, function (err) {
+  console.log('ipc-stream ended', err)
+})
+pull(ipcStream, someOtherStream, ipcStream)
+```
---
+++ index.js
@@ -0,0 +1,99 @@
+var pull  = require('pull-stream')
+
+function once (fn) {
+  var done = false
+  return function (err, val) {
+    if(done) return
+    done = true
+    fn(err, val)
+  }
+}
+
+module.exports = function (ipc, window, _done) {
+  var buffer = [], ended = false, waiting
+  var onincomingHandler
+
+  // handle (ipc, _done) call signature
+  if (typeof window == 'function' && !_done) {
+    _done = window
+    window = null
+  }
+
+  var done = once(function (err, v) {
+    ipc.removeListener('pull-ipc', onincomingHandler)
+    _done && _done(err, v)
+
+    // deallocate
+    _done = null
+    window = null
+    waiting = null
+  })
+
+  // incoming msg handler
+  function onincoming (msg) {
+    // parse if needed
+    try {
+      if (typeof msg == 'string')
+        msg = JSON.parse(msg)
+    } catch (e) {
+      return
+    }
+
+    if (msg.bvalue) {
+      // convert buffers to back to binary
+      msg.value = new Buffer(msg.bvalue, 'base64')
+      delete msg.bvalue
+    }
+
+    // send to pull-stream if it's waiting for data
+    // otherwise, buffer the data
+    if (waiting) {
+      var cb = waiting
+      waiting = null
+      cb(ended, msg)
+    }
+    else if (!ended)
+      buffer.push(msg)
+  }
+
+  // outgoing msg handler
+  function onoutgoing (msg) { 
+    if (msg.value && Buffer.isBuffer(msg.value)) {
+      // convert buffers to base64
+      msg.bvalue = msg.value.toString('base64')
+      delete msg.value
+    }
+    if (window) window.webContents.send('pull-ipc', JSON.stringify(msg))
+    else        ipc.send('pull-ipc', JSON.stringify(msg))
+  }
+
+  // handle incoming messages
+  onincomingHandler = (window) ?
+    (function (e, msg) {
+      if (e.sender !== window.webContents) return
+      onincoming(msg)
+    }) : onincoming
+  ipc.on('pull-ipc', onincomingHandler)
+
+  // return source/sink
+  return {
+    source: function (abort, cb) {
+      if (abort) {
+        cb(abort)
+        done(abort !== true ? abort : null)
+      }
+      else if (buffer.length) cb(null, buffer.shift())
+      else if (ended) cb(ended)
+      else waiting = cb
+    },
+    sink  : function (read) {
+      pull.drain(function (data) {
+        if (ended) return false
+        onoutgoing(data)
+      }, function (err) {
+        done && done(err)
+      })
+      (read)
+    }
+  }
+}
